17c44423f6085f3f98b6170acb405663d5ebd233
Abuk <abukcz@gmail.com>
2017-01-18 12:42:13 +0100

    Revert "Bullhead: Enable Snap Camera"
    This reverts commit 54eac05e22bc9f3ded5f76cd1a3d3ba7c4669094.

--- device.mk
+++ device.mk
@@ -228,8 +228,7 @@ PRODUCT_PACKAGES += \
     libmmcamera_interface2 \
     libmmjpeg_interface \
     libqomx_core \
-    mm-qcamera-app \
-    Snap
+    mm-qcamera-app
 
 # Sensor & activity_recognition HAL
 TARGET_USES_NANOHUB_SENSORHAL := true
