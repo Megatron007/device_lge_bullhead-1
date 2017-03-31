e5d07c525a519c92519486f9cfdfbaefc435292f
Omkar <djomi400@gmail.com>
2017-03-31 14:32:58 +0530

    bullhead: use lineage kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,10 +32,9 @@ TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
 
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := pizza_defconfig
+TARGET_KERNEL_CONFIG := lineageos_bullhead_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
 TARGET_USES_UNCOMPRESSED_KERNEL := true
